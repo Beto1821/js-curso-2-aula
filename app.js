e69845cc347b9f54baf50864729e8057d47590eb
Adalberto R. Ribeiro <beto1821@uol.com.br>
2024-01-24 23:03:13 -0300

    feat: reponsiviVoice

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let tentativas = 1;
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    // voice
     responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
 
